7bade6bbfb9aacf4226f80e78d47adb369498b42
Shreyas Jadhav <sshreyasjadhav2021@gmail.com>
2025-01-28 21:54:35 +0530

    Add files via upload

---
+++ APP.java
@@ -0,0 +1,9 @@
+//: c13:Applet1.java
+// Very simple applet
+import javax.swing.*;
+import java.awt.*;
+public class App extends JApplet {
+public void init() {
+getContentPane().add(new JLabel("Applet!"));
+}
+} ///:~
